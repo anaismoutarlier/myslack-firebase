0cd8fa9d74781e868ec638031b21b750f8e22e4b
Anais Moutarlier <anaismoutarlier@gmail.com>
2023-01-20 10:16:34 +0100

    add firestore

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getFirestore,
+  addDoc,
+  setDoc,
+  collection,
+  doc,
+} from "firebase/firestore";
 
 import FirebaseContext from "./context";
 import useAuth from "./useAuth";
 
+/**
+ * Class that handles connections with a Firebase setup
+ * @param {Object} config Configuration information provided by Firebase
+ */
 class Firebase {
   constructor(config) {
     this.app = initializeApp(config);
     this.auth = getAuth(this.app);
+    this.db = getFirestore(this.app);
     this.googleProvider = new GoogleAuthProvider();
   }
 
@@ -15,7 +27,15 @@ class Firebase {
     const { auth, googleProvider } = this;
     await signInWithPopup(auth, googleProvider);
   }
+
+  async saveUser(uid, user) {
+    const { db } = this;
+
+    const userRef = doc(db, "users", uid);
+
+    setDoc(userRef, user, { merge: true });
+  }
 }
 
 export default Firebase;
-export { FirebaseContext, useAuth }
+export { FirebaseContext, useAuth };
--- src/firebase/useAuth.js
+++ src/firebase/useAuth.js
@@ -8,6 +8,18 @@ export default function useAuth() {
 
   useEffect(() => {
     const unsubscribe = firebase.auth.onAuthStateChanged(data => {
+      if (data) {
+        const newUser = {
+          // email: data.email,
+          // displayName: data.displayName,
+          // photoURL: data.photoURL,
+          // lastLogged: new Date(),
+          online: true
+        }
+        firebase.saveUser(data.uid, newUser);
+      } else {
+        firebase.saveUser(user.uid, { online: false });
+      }
       setUser(data);
     });
 
